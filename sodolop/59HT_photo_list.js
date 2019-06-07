d193c6063aadd738b012f90ca4f336cce178bcd3
vuthuy96 <50038030+vuthuy96@users.noreply.github.com>
2019-06-07 17:39:56 +0700

    p1_BTL

---
+++ sodolop/59HT_photo_list.js
@@ -0,0 +1,14 @@
+const PHOTO_LIST=
+[
+    'img/apple.jpg',
+    'img/gv_KTD.jpg',
+    'img/nguyenanh.jpg',
+    'img/hoanganh.jpg',
+    'img/hnav.jpg',
+    'img/nguyenchien.jpg',
+    'img/cumanhdat.jpg',
+    'img/ndmd.jpg',
+    'img/nguyendung.png'
+    
+   
+]
